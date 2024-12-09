452893a4b11b954c76269457bb169508dd616093
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2024-12-09 06:59:51 +0100

    Create migration & car entity

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
+use App\Form\CarType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,8 +21,12 @@ class CarController extends AbstractController
     #[Route('/cars/add', name: 'cars.add')]
     public function add(): Response
     {
+        $car = new Car();
+        $form = $this->createForm(CarType::class, $car);
+
         return $this->render('car/add.html.twig', [
             'controller_name' => 'CarController',
+            'form' => $form,
         ]);
     }
 }
